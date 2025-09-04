e70ed5fa3926193abd13964e0c82c4c87ab76def
COMRADE040 <rafiulislam040@gmail.com>
2025-09-04 20:03:35 +0600

    added card section

--- script/script.js
+++ script/script.js
@@ -7,6 +7,28 @@ const loadLessons = () => {
     .then(json => displayLesson(json.data))
 
 }
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLevelWord(data.data))
+}
+
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById('word-container')
+    // wordContainer.innerHTML = "";
+
+    words.forEach((word) => {
+        const card = document.createElement('div')
+        card.innerHTML = `
+        <p> Cat </p>
+        `;
+        wordContainer.append(card)
+    })
+}
+
+
 const displayLesson = (lessons) => {
     // 1.Get The Container & Container Will Be Empty
     const levelContainer = document.getElementById('level-container');
@@ -15,7 +37,7 @@ const displayLesson = (lessons) => {
     for(let lesson of lessons){
     // 3.Create Element
     const btnDiv = document.createElement('div')
-    btnDiv.innerHTML = `<button class="btn btn-outline btn-primary">
+    btnDiv.innerHTML = `<button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
     <a href="" class=""><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</a>
     </button>`;
     // 4.Append Into Container
